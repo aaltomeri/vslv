614ab547a0a17778b52dbcb38325d9c5f300489c
Thomas Mery <thomas.mery@gmail.com>
2013-12-22 17:02:08 +0100

    removed Sidebar in main layout

--- wp-content/themes/vslv/base.php
+++ wp-content/themes/vslv/base.php
@@ -20,13 +20,9 @@
   <div class="wrap container" role="document">
     <div class="content row">
       <main class="main <?php echo roots_main_class(); ?>" role="main">
+        <?php var_dump(Roots_Wrapping::$main_template); ?>
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
     </div><!-- /.content -->
   </div><!-- /.wrap -->
 
