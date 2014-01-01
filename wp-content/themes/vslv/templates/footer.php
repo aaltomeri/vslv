90750767623b09c396d0840bc668c63f7723afca
Thomas Mery <thomas.mery@gmail.com>
2014-01-01 14:59:47 +0100

    cleaned footer

--- wp-content/themes/vslv/templates/footer.php
+++ wp-content/themes/vslv/templates/footer.php
@@ -1,9 +1,6 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-    </div>
+    <div class="col-lg-12"></div>
   </div>
 </footer>
 
