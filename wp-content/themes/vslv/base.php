a27c89ce4515ca99bf888afccac82ecee5c0b9b7
Thomas Mery <thomas.mery@gmail.com>
2014-01-14 13:08:14 +0100

    style content panel - basics

--- wp-content/themes/vslv/base.php
+++ wp-content/themes/vslv/base.php
@@ -24,12 +24,18 @@
     <div id="discovery"></div>
     <div id="content" class="is-hidden">
       <script type="text/template" id="content-template">
-        <div class="content">
+        <div class="content <%= data.type %>">
           <span class="hi-response hi-response-remove toggle"></span>
+          <span class="hi-response hi-response-down-arrow scroll-hint"></span>
           <% if(data.type == 'project') { %>
             <h1><%= data.title %></h1>
           <% } %>
-          <p><%= data.content %></p>
+          <% if(data.content !== '') { %>
+            <%= data.content %>
+          <% } else { %>
+            <h3>Cras mattis consectetur purus sit amet fermentum. Maecenas sed diam eget risus varius blandit sit amet non magna.</h3>
+            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur.</p>
+          <% } %>
         </div>
       </script>
     </div>
--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -72,7 +72,7 @@ add_action( 'json_prepare_post', 'vslv_augment_posts_json_with_attachments', 10,
  */
 function roots_scripts() {
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8130746f3a601430f563d91736f7844f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5cd5b5f96903526723ff8c11b247f769');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
