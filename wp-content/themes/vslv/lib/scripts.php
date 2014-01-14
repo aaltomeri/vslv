2996a9d546ab6cce2a1d9d9b5dca54da3ecce0b0
Thomas Mery <thomas.mery@gmail.com>
2014-01-14 15:35:42 +0100

    fixed css for open panel icon

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -72,7 +72,7 @@ add_action( 'json_prepare_post', 'vslv_augment_posts_json_with_attachments', 10,
  */
 function roots_scripts() {
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '634101cde90a89587275b3383bf06d29');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '724cc19520433b11d05af2cdda7e6a93');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
