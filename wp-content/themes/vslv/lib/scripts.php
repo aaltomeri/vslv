1f6dcd7a45e8c0bca8997f56ea7b18f371f1320b
Thomas Mery <thomas.mery@gmail.com>
2014-01-27 16:45:32 +0100

    slide cursor on portfolio

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -72,7 +72,7 @@ add_action( 'json_prepare_post', 'vslv_augment_posts_json_with_attachments', 10,
  */
 function roots_scripts() {
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b6336a84d8bb7cf3a4bdf0c727dd130f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9ec99111088515e0ec97aca863111666');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
