0a98de68359ae50e9c8a13891f38c14784480830
Thomas Mery <thomas.mery@gmail.com>
2014-02-08 18:20:18 +0100

    added compiled css

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -94,7 +94,7 @@ add_action( 'json_prepare_attachment', 'vslv_augment_videos_json_with_additional
  */
 function roots_scripts() {
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c1d42e2353770f7ca7a6c3563f562ee9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '414dd53293e8a4b838435011bba007f1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
