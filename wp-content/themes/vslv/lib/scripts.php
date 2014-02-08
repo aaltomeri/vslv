a9467118c104b165968e2e4814dc53ffc62fa56d
Thomas Mery <thomas.mery@gmail.com>
2014-02-08 20:22:09 +0100

    implement RWD styles - closes #71

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -94,7 +94,7 @@ add_action( 'json_prepare_attachment', 'vslv_augment_videos_json_with_additional
  */
 function roots_scripts() {
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c1d42e2353770f7ca7a6c3563f562ee9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9b959eab21529da1710a15f04cce0636');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
@@ -110,7 +110,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'e14e3ec7514978343901748fc6a16043', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '80f325d1f516859c26dc5faca49acff1', true);
 
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
