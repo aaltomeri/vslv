8d4601b432fd7014153e6ddc6614ebed6612201c
Thomas Mery <thomas.mery@gmail.com>
2014-02-09 19:33:01 +0100

    added custom styles in wp editor

--- wp-content/themes/vslv/lib/custom.php
+++ wp-content/themes/vslv/lib/custom.php
@@ -304,6 +304,60 @@ function VSLV_make_video_formats_tags($sources) {
 
 }
 
+/**
+ * WP EDITOR CUSTOM STYLES
+ */
+
+// Callback function to insert 'styleselect' into the $buttons array
+function vslv_mce_buttons_2( $buttons ) {
+  array_unshift( $buttons, 'styleselect' );
+  return $buttons;
+}
+// Register our callback to the appropriate filter
+add_filter('mce_buttons_2', 'vslv_mce_buttons_2');
+
+// Callback function to filter the MCE settings
+function vslv_mce_before_init_insert_formats( $init_array ) {  
+  // Define the style_formats array
+  $style_formats = array(  
+    // Each array child is a format with it's own settings
+    array(  
+
+      'title' => '.contact1',  
+      'block' => 'div',  
+      'classes' => 'contact1',
+      'wrapper' => true,
+      
+    ), 
+    array(  
+      
+      'title' => '.contact2',  
+      'block' => 'div',  
+      'classes' => 'contact2',
+      'wrapper' => true,
+      
+    ), 
+    array(  
+      
+      'title' => '.contact3',  
+      'block' => 'div',  
+      'classes' => 'contact3',
+      'wrapper' => true,
+      
+    ),
+    
+  );  
+  // Insert the array, JSON ENCODED, into 'style_formats'
+  $init_array['style_formats'] = json_encode( $style_formats );  
+  
+  return $init_array;  
+  
+} 
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'vslv_mce_before_init_insert_formats' );
+
+
+
 
 
 
