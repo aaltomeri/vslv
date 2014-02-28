6b24d54bbf678bdb9d4badeee6ac282561c1669e
Thomas Mery <thomas.mery@gmail.com>
2014-02-28 17:21:25 +0100

    css for Agence center panel

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -159,7 +159,7 @@ add_action( 'json_prepare_attachment', 'vslv_augment_videos_json_with_additional
 function roots_scripts() {
 
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '39d5be0db53333f6371a8bc32cdf60aa');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '603dfa564c49954902e25cc9eada0df3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
