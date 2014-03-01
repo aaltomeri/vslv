27ca3cfa38f92c2ac47465fb5bb687f0054cb4ba
Thomas Mery <thomas.mery@gmail.com>
2014-03-01 17:46:49 +0100

    css fix for client list on mobile

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -159,7 +159,7 @@ add_action( 'json_prepare_attachment', 'vslv_augment_videos_json_with_additional
 function roots_scripts() {
 
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1197723909bb1e73c72cfeb9a693241a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '63664e3b72a3f21b236c4e1e516f963d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
