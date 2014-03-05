b983ecaf8d715d315265b7430cc8d57227f6fe61
Thomas Mery <thomas.mery@gmail.com>
2014-03-05 11:02:17 +0100

    commented logging

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -175,7 +175,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '22959f1c3f280540da9aa9f8fd1192a8', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '09c2f38b762f2aa15b39a4d8bbf6adab', true);
 
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
