33363b1de4e5070a9cd1199c68f8dfa150a4a24f
Thomas Mery <thomas.mery@gmail.com>
2014-03-26 17:33:59 +0100

    build

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -175,7 +175,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'aea32268899cc2b8e4ef1654c3ef0dd6', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'a9d7749fa9df7c601c532760776f020f', true);
 
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
