a35e790d6dcfa1a5495438b740041adcf2243250
Thomas Mery <thomas.mery@gmail.com>
2014-03-28 00:03:14 +0100

    fix for portfolio thumbnails not appearing in ie

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -193,7 +193,7 @@ add_action( 'json_prepare_attachment', 'vslv_augment_videos_json_with_additional
 function roots_scripts() {
 
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ff74bc25b8b7204044282b1253a3a6c8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c5edfea64f486b4f85807e6ce801b66c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
