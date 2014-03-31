440c4887653267c87f018ea7bbd4f5a0740b43e8
Thomas Mery <thomas.mery@gmail.com>
2014-03-31 09:47:27 +0200

    fixed portfolio items not appearing on Safari 5
    Hopefully this won’t break anything … display: inline for portfolio
    items was working fine and has some advantages at the time - reverting
    to inline-block makes it work in Safari 5 but might bring back some
    other problems

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -193,7 +193,7 @@ add_action( 'json_prepare_attachment', 'vslv_augment_videos_json_with_additional
 function roots_scripts() {
 
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c5edfea64f486b4f85807e6ce801b66c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e919bf3fb3fa627e397c07167297f30c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
