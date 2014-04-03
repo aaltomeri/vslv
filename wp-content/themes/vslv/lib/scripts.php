4cad2e86d289f35f5ba64a03942dbc47326bd639
Thomas Mery <thomas.mery@gmail.com>
2014-04-03 09:32:33 +0200

    follow up to clients list styles changes

--- wp-content/themes/vslv/lib/scripts.php
+++ wp-content/themes/vslv/lib/scripts.php
@@ -193,7 +193,7 @@ add_action( 'json_prepare_attachment', 'vslv_augment_videos_json_with_additional
 function roots_scripts() {
 
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '280537a2340173975a50c345af7c702a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '983344913e4e1e53c690219ae5e5cc18');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
